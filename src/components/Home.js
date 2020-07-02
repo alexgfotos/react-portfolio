9c8b9ae7eca8bbca363923a7fea0e9ca37d0f92c
alex g <alexgfotos@gmail.com>
2020-07-02 09:00:21 -0700

    site title and icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,15 @@ export const Home = () => {
     return (
         <Container maxWidth="lg">
         
-                <Box
+                {/* <Box
                     p={0}
                     position="relative"
                     top={10}
                     left=""
                     width={"25%"}
                 ><img style={{ width: "100%" }} src="assets/images/me and mina.jpg" alt="me and my dog Mina doing it for the gram"></img>
-                </Box>
+                </Box> */}
+                
                 <p><strong>Hello!</strong> My name is Alejandro Gonzalez and welcome to my portfolio site. I currently work as a
         Photography Instructor, Career & Technical Education department chair, Web Editor, and Teacher Technology
         Liaison at Tucson High Magnet School. I am also a freelance photographer, cacti aficionado, and soon to be a
